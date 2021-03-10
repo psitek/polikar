019ed5a16571cd3811847284a859561a19b194dd
Weronika <weronika.szafinska@havas.com>
2021-03-10 14:28:10 +0000

    Update footer and header

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,28 +1,139 @@
-import React from "react";
+import React from "react"
 
-const Footer = () => (
-  <footer className="pt-48">
-    <div>
-      <h2>Skontaktuj się z nami!</h2>
-      <p>
-        POLIKAR Karolina Futerska
-        64-700 Kuźnica Czarnkowska ul. Myśliwska 2a
-        NIP 763-207-46-40
-      </p>
-      <p>firmapolikar1@outlook.com</p>
-      <p>
-        Adres Produkcji
-        64-920 Piła ul. Pilotów 7
-      </p>
-    </div>
-    <div>
-      <p>
-        Znajdz nas na
-        Facebook
-        Instagram
-      </p>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  const date = new Date()
+  return (
+    <footer className="relative w-full mt-48 bg-gray-200">
+      <div className="relative w-full bg-gray-200 footer-box"></div>
+      <section className="flex flex-col px-4">
+        <h2 className="sm:text-center">Skontaktuj się z nami!</h2>
+        <div className="flex flex-col sm:justify-around sm:flex-row">
+          <div className="flex items-start pb-2 mt-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-3 feather feather-home"
+            >
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+            <div>
+              <p className="block">
+                POLIKAR Karolina Futerska
+                <span className="block">ul. Myśliwska 2a</span>
+                <span className="block">64-700 Kuźnica Czarnkowska</span>
+                <span className="block">NIP 763-207-46-40</span>
+              </p>
+            </div>
+          </div>
 
-export default Footer;
+          <div className="flex items-start pb-2 mt-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-3 feather feather-truck"
+            >
+              <rect x="1" y="3" width="15" height="13"></rect>
+              <polygon points="16 8 20 8 23 11 23 16 16 16 16 8"></polygon>
+              <circle cx="5.5" cy="18.5" r="2.5"></circle>
+              <circle cx="18.5" cy="18.5" r="2.5"></circle>
+            </svg>
+            <p className="block">
+              Adres Produkcji
+              <span className="block">ul. Pilotów 7</span>
+              <span className="block">64-920 Piła</span>
+            </p>
+          </div>
+
+          <div className="flex items-start pb-2 mt-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-3 feather feather-mail"
+            >
+              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+              <polyline points="22,6 12,13 2,6"></polyline>
+            </svg>
+            <div className="">
+              <a href="mailto:firmapolikar1@outlook.com">
+                firmapolikar1@outlook.com
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-center pb-2 mt-4">
+          <p className="mr-2 text-base font-bold uppercase">Znajdź nas na: </p>
+          <div className="flex">
+            <a
+              href="https://www.facebook.com/POLIKAR.POLIGRAFIA/"
+              className="py-3 pr-3 hover:text-brandBlue"
+            >
+              <svg
+                class="h-8 w-8"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+
+            <a
+              href="https://www.instagram.com/polikarpoligrafia/"
+              className="py-3 hover:text-brandBlue"
+            >
+              <span className="sr-only">Instagram</span>
+              <svg
+                class="h-8 w-8"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <div className="px-4 py-4 mx-auto border-t border-gray-700 sm:w-full sm:flex sm:justify-center max-w-7xl sm:px-6 lg:py-16 lg:px-8">
+          <p className="mt-8 text-base md:mt-0 md:order-1">
+            &copy;{" "}
+            {`
+           ${date.getFullYear()} Polikar. All rights reserved.`}
+          </p>
+        </div>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,51 +1,81 @@
-import React, { useState } from "react";
-import { Helmet } from "react-helmet";
-import { Link } from "gatsby";
-import polikarLogo from "../images/polikar-logo.png";
+import React, { useState } from "react"
+import { Helmet } from "react-helmet"
+import { Link } from "gatsby"
+import polikarLogo from "../images/polikar-logo.png"
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <header className="container px-4 mx-auto mt-2">
+    <header className="container px-4 mx-auto mt-2 mb-8 sm:mb-0">
       <Helmet>
         <link href="https://fonts.googleapis.com/css?family=Nova+Flat:400|Karma:400|Montserrat:wght@100;400;600;700" />
       </Helmet>
-      <div className="flex justify-between">
+      <div className="flex justify-between sm:justify-start align-items">
         <img className="w-40 md:w-56" src={polikarLogo} alt="Polikar logo" />
-        <nav className="flex items-center md:items-baseline md:w-5/12">
-
-          <div className={`${menuOpen ? "z-0" : "z-10"} block lg:hidden relative`}>
-            <button className="flex items-center justify-center w-10 h-8 mr-2 border-2 rounded md:hidden text-brandNeutral border-brandNeutral hover:text-gray-700 hover:border-gray-700" onClick={() => setMenuOpen(true)}>
-              <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <nav className="flex items-center ml-8 md:w-5/12">
+          <div
+            className={`${menuOpen ? "z-0" : "z-10"} block lg:hidden relative`}
+          >
+            <button
+              className="flex items-center justify-center w-10 h-8 mr-2 border-2 rounded md:hidden text-brandBlue border-brandBlue hover:text-gray-700 hover:border-gray-700"
+              onClick={() => setMenuOpen(true)}
+            >
+              <svg
+                className="w-4 h-4 fill-current"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <title>Menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
               </svg>
             </button>
           </div>
 
-          <div className={`${menuOpen ? "opacity-100" : "opacity-0"} md:opacity-100 absolute md:relative px-4 w-full right-0 top-4 transition-opacity duration-100 ease-in md:p-0 `}>
+          <div
+            className={`${
+              menuOpen ? "opacity-100" : "opacity-0"
+            } md:opacity-100 absolute md:relative px-4 w-full right-0 top-4 transition-opacity duration-100 ease-in md:p-0 `}
+          >
             <div className="container">
               <div className="flex justify-between mt-24 text-left bg-white rounded shadow md:bg-transparent md:border-none md:shadow-none md:flex-row md:mt-0">
                 <ul className="flex flex-col py-4 pt-4 ml-8 md:flex-row md:p-0 md:m-0 md:w-full md:justify-between">
-                  <li className="w-20 py-2 text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900">Oferta</li>
-                  <li className="w-20 py-2 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900">O nas</li>
-                  <li className="w-20 py-2 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900">Kontakt</li>
+                  <li className="w-20 py-2 text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900">
+                    Oferta
+                  </li>
+                  <li className="w-20 py-2 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900">
+                    O nas
+                  </li>
+                  <li className="w-20 py-2 font-medium text-gray-500 transition duration-150 ease-in-out hover:text-gray-900 focus:outline-none focus:text-gray-900">
+                    Kontakt
+                  </li>
                 </ul>
-                <button type="button" className="inline-flex items-center justify-center w-10 h-8 mt-4 mr-2 text-gray-700 border-2 border-gray-500 rounded hover:text-gray-700 hover:border-gray-700 md:hidden" onClick={() => setMenuOpen(false)}>
-                  <svg className="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center w-10 h-8 mt-4 mr-2 border-2 rounded border-brandBlue text-brandBlue hover:text-brandBlue hover:border-brandBlue md:hidden"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  <svg
+                    className="w-6 h-6"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
                   </svg>
                 </button>
               </div>
             </div>
-
           </div>
-
         </nav>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
